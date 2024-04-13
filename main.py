a5d8433ee1bc725c7d4b85737904f220dfe7995c
kishan-chakraborty <kishanchakraborty833@gmail.com>
2024-04-13 12:59:55 +0530

    chat bot is done. stream lit part is required.

--- main.py
+++ main.py
@@ -42,3 +42,21 @@ if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question !"}
     ]
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): 
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: 
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            print(response, show_source=True)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) 
